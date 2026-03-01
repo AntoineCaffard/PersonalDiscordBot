8d6ecdbde137056767df5204a6024c9040e975b9
AntoineCaffard <caffard.antoine@gmail.com>
2026-03-01 03:09:55 +0100

    Better Bot version

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+import asyncio
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(command_prefix="!", intents=intents)
+        self.guild_id = int(os.getenv("GUILD_ID"))
+
+    async def setup_hook(self):
+        await self.load_extension("cogs.diceCog")
+        print("✅ diceCog chargé")
+        guild = discord.Object(id=self.guild_id)
+        synced = await self.tree.sync(guild=guild)
+        print(f"✅ {len(synced)} commandes synchronisées sur le serveur test")
+        for cmd in synced:
+            print("-", cmd.name)
+
+    async def on_ready(self):
+        print(f"Connecté en tant que {self.user} (ID: {self.user.id})")
+
+async def run():
+    token = os.getenv("BOT_TOKEN")
+    async with MyBot() as bot:
+        await bot.start(token)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(run())
+    except KeyboardInterrupt:
+        print("\n🛑 Bot arrêté proprement")
---
+++ utils/embed_creator.py
@@ -0,0 +1,23 @@
+import discord
+
+
+def colorize(n, max_val, mod=0):
+    if n == 1:
+        return f"🔴 {n}"
+    elif n == max_val + mod:
+        return f"🟢 {n}"
+    else:
+        return f"⚪ {n}"
+
+def dice_result_embed(user, num_dice, num_faces, modifier, results_raw, results, color=discord.Color.blue()):
+    
+    mod_str = f"+{modifier}" if modifier > 0 else (f"{modifier}" if modifier < 0 else "0")
+    raw_str = ", ".join(str(n) for n in results_raw)
+    table_str = "\n".join(colorize(n, num_faces, modifier) for n in results)
+    table_str = f"```\n{table_str}\n```"
+
+    embed = discord.Embed(title=f"{user.display_name} a lancé {num_dice}d{num_faces}", color=color)
+    embed.add_field(name="Modificateur", value=mod_str, inline=False)
+    embed.add_field(name="Valeurs", value=f"```\n{results_raw}\n```", inline=False)
+    embed.add_field(name="Résultat", value=table_str, inline=False)
+    return embed
